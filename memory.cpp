9e0390fba8279b9d499587efc463fe2cedf568dc
Raymond W. Ko <raymond.w.ko@gmail.com>
2010-04-18 06:04:54 +0000

    Fixed warnings

--- memory.cpp
+++ memory.cpp
@@ -7,19 +7,19 @@ LC4Machine::perform_load(unsigned short op)
     unsigned short d = extract(op, 9, 11);
     short offset = ((short) SEXT(extract(op, 0, 5), 6)) * sizeof(short);
 
-    void* ptr = this->memory;
+    unsigned char* ptr = (unsigned char*) this->memory;
     unsigned short addr = this->regs.r[s];
 
     // Check for out of bound memory access to prevent buffer overflows
     if (this->MEMCHECK) {
-        void* test_ptr = (void*) &this->memory[addr];
+        unsigned char* test_ptr = (unsigned char*) &this->memory[addr];
         if (test_ptr + offset > (test_ptr + 0xFFFF + 1)) {
             throw "Out of bound read error!";
             return 0xDEAD;
         }
     }
 
-    ptr = &this->memory[addr];
+    ptr = (unsigned char*)&this->memory[addr];
     ptr += offset;
     this->regs.r[d] = *((unsigned short*) ptr);
 
@@ -33,19 +33,19 @@ LC4Machine::perform_store(unsigned short op)
     unsigned short d = extract(op, 9, 11);
     short offset = ((short) SEXT(extract(op, 0, 5), 6)) * sizeof(short);
 
-    void* ptr = this->memory;
+    unsigned char* ptr = (unsigned char*) this->memory;
     unsigned short addr = this->regs.r[s];
 
     // Check for out of bound memory access to prevent buffer overflows
     if (this->MEMCHECK) {
-        void* test_ptr = (void*) &this->memory[addr];
+        unsigned char* test_ptr = (unsigned char*) &this->memory[addr];
         if (test_ptr + offset > (test_ptr + 0xFFFF + 1)) {
             throw "Out of bound read error!";
             return 0xDEAD;
         }
     }
 
-    ptr = &this->memory[addr];
+    ptr = (unsigned char*)&this->memory[addr];
     ptr += offset;
     *((unsigned short*) ptr) = this->regs.r[d];
 
