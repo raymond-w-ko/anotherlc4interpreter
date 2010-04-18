8ec9af9c987b2bb16170e06ad1fbb3245aecea38
Raymond.W.Ko <Raymond.W.Ko@0d3d2af3-02ef-9f9f-d0bf-ebaf8f2d8941>
2010-04-18 05:54:15 +0000

    Spy creeping around here...

---
+++ trunk/ali.cpp
@@ -0,0 +1,14 @@
+﻿#include "LC4Machine.h"
+#include "ncurses.h"
+
+int main(int argc, char* argv[])
+{
+    LC4Machine lc4("test.bin");
+
+    // initialized ncurses
+    initscr();
+
+    // cleanup ncurses
+    endwin();
+    return 0;
+}
